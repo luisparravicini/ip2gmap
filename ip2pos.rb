cd04ce17d7212fb03de03cc66b27c8b4251bda20
Luis Parravicini <lparravi@gmail.com>
2012-02-17 19:59:52 -0300

    indent

--- ip2pos.rb
+++ ip2pos.rb
@@ -5,7 +5,7 @@ File.open('data.json', 'w') do |io_out|
   File.open(path) do |io|
     io.each_line do |line|
       out = `geoiplookup '#{line.strip}'`
-out.force_encoding('iso-8859-1')
+      out.force_encoding('iso-8859-1')
 
       pos = out.scan(/(-?\d+\.\d+), (-?\d+\.\d+)/).uniq
       pos.each { |x| io_out.puts("  [%s, %s]," % x) }
